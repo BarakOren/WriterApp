9f85328ee91ab4750243131f628f759a97190dd2
BarakOren <barakoren5@gmail.com>
2022-06-17 00:13:46 +0300

    update

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -9,16 +9,13 @@ const Header = () => {
     const location = useLocation().pathname;
 
     return(
-        <div>    
-            <div className="header">
+        <div className="header" style={{backgroundColor: location === "/" ?  "unset" : "#1C1C23"}}>
             <p className="name">Writer App</p>
-            <div className="items">
+            <div class="center">
             <Link style={{color: location === "/" ? "white" : ""}} to="/" className="headerItem">Home</Link>
             <Link style={{color: location === "/about" ? "white" : ""}} to="/about" className="headerItem">About</Link>
-            <Link style={{color: location === "/contact" ? "white" : ""}} to="/contact" className="headerItem">Contact</Link>
-            <Link onClick={() => window.scrollTo({top: 0, behavior: 'smooth'})} style={{color: location === "/web-version" || location === "/saved" ? "white" : ""}} to="/web-version" className="headerItem">Web-version</Link>
             </div>
-            </div>
+            <Link class="web-version" onClick={() => window.scrollTo({top: 0, behavior: 'smooth'})} style={{color: location === "/web-version" || location === "/saved" ? "white" : ""}} to="/web-version" className="headerItem">Web-version</Link>
         </div>
 
     )
--- src/components/homepage/homepage.component.jsx
+++ src/components/homepage/homepage.component.jsx
@@ -1,14 +1,11 @@
 import React from "react";
 import "./homepage.styles.scss";
 import Iphone from "../../assets/noshadow.png";
-import ShopIcon from '@material-ui/icons/Shop';
-import LanguageIcon from '@material-ui/icons/Language';
-import grey from '@material-ui/core/colors/grey';
 import { useHistory } from "react-router";
-
+import iphone from "../../assets/iPhone.png"
+import cubes from "../../assets/Cubes.png";
 
 const HomePage = () => {
-
     const history = useHistory()
 
     function goToWebVersion(){
@@ -22,53 +19,38 @@ const HomePage = () => {
 
     return (
         <div className="homepage">
-            <div className="first">
+            <div class="color-bomb-top"></div>
+            <div class="color-bomb-bottom"></div>
                 <div className="left">
                     <div>
-                    <h1 className="title">Writer App</h1>
+                    <h1 className="title">WRITER APP</h1>
                     <p className="subtitle">choose smarter.</p>
+                    </div>
+                    <div className="link-buttons">
+                        <button className="link-button" onClick={() => openGoogle()}>Google Play</button>
+                        <button className="link-button" onClick={() => goToWebVersion()}>Web Version</button>
+                    </div>
+                    <div className="intro">
+                        <p>Writer contains all the spray-paint shades in the industry. simply choose the combination from your phone.</p>
                     </div>
                 </div>
 
                 <div className="right">
-                    <img className="iphone" src={Iphone} alt="iphone" />
-                    <div className="comingsoon">
-                        <div 
-                        onClick={() => openGoogle()}
-                            className="box">
-                            <ShopIcon className="icon" style={{fontSize: 30, color: grey[100]}} />
-                            <p className="coming">Google Play</p>
-                        </div>
-                        {/* <div className="box">
-                            <AppleIcon className="icon" style={{fontSize: 30, color: grey[700]}} />
-                            <p className="coming">COMING SOON</p>
-                        </div> */}
-                        <div className="box" onClick={() => goToWebVersion()}>
-                            <LanguageIcon className="icon" style={{fontSize: 30, color: grey[100]}} />
-                            <p className="coming">Web Version</p>
-                        </div>
+                    <div className="image-container">
+                        <p className="free">Free</p>
+                        <img className="iphone" src={iphone} alt="iphone" />
                     </div>
-                </div>
+                    <div class="cubes-container">
+                        <div className="fader"></div>
+                        <img className="cubes" src={cubes} alt="effect" />
+                    </div>
+                    <div class="mobile-cubes-container">
+                        <div className="fader"></div>
+                        <img className="cubes" src={cubes} alt="effect" />
+                    </div>
+                    </div>
 
-            </div> 
             {/* first end */}
-            <div className="second">
-                <div className="intro">
-                    <p>We have all been there, beginner or experts.
-                    <br />
-                    Staring at the paint for way too long, while the shop owner stares back at you.
-                    <br />
-                    trying to figure it out in our heads.
-                    <br />
-                    So many options, never sure if what you picked gonna work well.
-                    <br />
-                    <br />
-                    Writer App takes it away.
-                    <br />
-                    Writer contains all the spray-paint shades in the industry. simply choose the combination from your phone.
-                    </p>
-                </div>
-            </div>
             
         </div>
     )
